9867d17f2b94f972c602b630a3130229063d6931
Newton Hoac <nhoac001@ucr.edu>
2015-05-23 23:58:38 -0700

    ok

--- problems/p1/fizzbuzz.h
+++ problems/p1/fizzbuzz.h
@@ -1,5 +1,20 @@
 #include <string>
 static std::string fizzbuzz()
 {
-	return "";
+	string neat;
+	for(int i = 1; i < 101; i++) {
+		if(i % 15 == 0) {
+			neat += "FizzBuzz ";
+		}
+		else if(i % 5 == 0) {
+			neat += "Buzz ";
+		}
+		else if(i % 3 == 0) {
+			neat += "Fizz ";
+		}
+		else {
+			neat += to_string(i) + ' ';
+		}
+	}
+	return neat;
 }
